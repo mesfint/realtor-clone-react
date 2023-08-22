7c5549f2c6f24ec8c176de327daa9cab1fb49d48
Mesfin <mesfin@Mesfins-MBP.home>
2023-08-22 23:43:44 +0300

    creaye pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
    <>
-<h1 className='text-3xl  font-bold underline'>Hello World</h1>
-
+<Router>
+  
+  <Routes>
+    <Route path="/" element= {<Home />} ></Route>
+    <Route path="/profile" element= {<Profile />} ></Route>
+    <Route path="/sign-in" element= {<SignIn />} ></Route>
+    <Route path="/sign-up" element= {<SignUp />} ></Route>
+    <Route path="/forgot-password" element= {<ForgotPassword />} ></Route>
+    <Route path="/offers" element= {<Offers />} ></Route>
+  </Routes>
+</Router>
    </>
   );
 }
